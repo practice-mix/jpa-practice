355c2a232f7cfa93ae278af9385d380fafdedd1f
leongeroge <leongeroge@outlook.com>
2020-12-21 17:50:35 +0800

    SuppressWarnings

--- src/main/java/com/example/jpapractice/sakila/config/EnumSetTypeDescriptor.java
+++ src/main/java/com/example/jpapractice/sakila/config/EnumSetTypeDescriptor.java
@@ -13,70 +13,76 @@ import java.util.stream.Collectors;
 /**
  * https://joshefin.xyz/map-a-collection-of-enums-to-a-column-with-hibernate/
  */
+@SuppressWarnings("rawtypes")
 public class EnumSetTypeDescriptor extends AbstractTypeDescriptor<EnumSet> {
 
-    private static final String SEPARATOR = ",";
+	private static final String SEPARATOR = ",";
 
-    private Object[] enumConstants;
+	private final Object[] enumConstants;
 
-    protected EnumSetTypeDescriptor(Class enumClass) {
-        super(EnumSet.class);
-        this.enumConstants = enumClass.getEnumConstants();
-    }
+	@SuppressWarnings({"rawtypes"})
+	protected EnumSetTypeDescriptor(Class enumClass) {
+		super(EnumSet.class);
+		this.enumConstants = enumClass.getEnumConstants();
+	}
 
-    @Override
-    public String toString(EnumSet value) {
-        return (String) value.stream()
-                .map(Enum.class::cast)
-                .map(v -> Integer.toString(((Enum) v).ordinal()))
-                .collect(Collectors.joining(SEPARATOR));
-    }
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	@Override
+	public String toString(EnumSet value) {
+		return (String) value.stream()
+				.map(Enum.class::cast)
+				.map(v -> Integer.toString(((Enum) v).ordinal()))
+				.collect(Collectors.joining(SEPARATOR));
+	}
 
-    @Override
-    public EnumSet fromString(String string) {
-        if (StringUtils.isEmpty(string))
-            return null;
+	@SuppressWarnings({"rawtypes", "ConstantConditions", "unchecked"})
+	@Override
+	public EnumSet fromString(String string) {
+		if (string == null)
+			return null;
 
-        List<Enum> list = Arrays.stream(StringUtils.split(string, SEPARATOR))
-                .map(ordinal -> enumConstants[Integer.parseInt(ordinal)])
-                .map(Enum.class::cast)
-                .collect(Collectors.toList());
+		List<Enum> list = Arrays.stream(StringUtils.split(string, SEPARATOR))
+				.map(ordinal -> enumConstants[Integer.parseInt(ordinal)])
+				.map(Enum.class::cast)
+				.collect(Collectors.toList());
 
-        if (list.isEmpty())
-            return null;
+		if (list.isEmpty())
+			return null;
 
-        return EnumSet.copyOf(list);
-    }
+		return EnumSet.copyOf(list);
+	}
 
-    @Override
-    public int extractHashCode(EnumSet value) {
-        return Objects.hashCode(value);
-    }
+	@Override
+	public int extractHashCode(EnumSet value) {
+		return Objects.hashCode(value);
+	}
 
-    @Override
-    public <X> X unwrap(EnumSet value, Class<X> type, WrapperOptions options) {
-        if (value == null)
-            return null;
+	@SuppressWarnings("unchecked")
+	@Override
+	public <X> X unwrap(EnumSet value, Class<X> type, WrapperOptions options) {
+		if (value == null)
+			return null;
 
-        if (EnumSet.class.isAssignableFrom(type))
-            return (X) value;
+		if (EnumSet.class.isAssignableFrom(type))
+			return (X) value;
 
-        if (String.class.isAssignableFrom(type))
-            return (X) toString(value);
+		if (String.class.isAssignableFrom(type))
+			return (X) toString(value);
 
-        throw unknownUnwrap(type);
-    }
+		throw unknownUnwrap(type);
+	}
 
-    @Override
-    public <X> EnumSet wrap(X value, WrapperOptions options) {
-        if (value == null)
-            return null;
+	@SuppressWarnings({"RedundantClassCall", "rawtypes"})
+	@Override
+	public <X> EnumSet wrap(X value, WrapperOptions options) {
+		if (value == null)
+			return null;
 
-        if (EnumSet.class.isInstance(value))
-            return (EnumSet) value;
+		if (EnumSet.class.isInstance(value))
+			return (EnumSet) value;
 
-        if (String.class.isInstance(value))
-            return fromString((String) value);
+		if (String.class.isInstance(value))
+			return fromString((String) value);
 
         throw unknownWrap(value.getClass());
     }
